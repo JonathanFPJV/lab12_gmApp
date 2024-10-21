6304e382b7af3ea001bb0250be164315417d7971
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2024-10-21 14:06:09 -0500

    Modificación de icono de posición

--- app/src/main/java/com/example/lab12_gm/MapScreen.kt
+++ app/src/main/java/com/example/lab12_gm/MapScreen.kt
@@ -1,11 +1,14 @@
 package com.example.lab12_gm
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -28,10 +31,14 @@ fun MapScreen() {
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
+            val context = LocalContext.current
+            val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.monicon)
+            val smallMarkerIcon = Bitmap.createScaledBitmap(bitmap, 84, 84, false) // Redimensiona el bitmap
+            val markerDescriptor = BitmapDescriptorFactory.fromBitmap(smallMarkerIcon)
             // Añadir marcador en Arequipa Perú
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
-                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE), // Icono azul
+                icon = markerDescriptor, // Icono personalizado
                 title = "Arequipa, Perú"
             )
 
