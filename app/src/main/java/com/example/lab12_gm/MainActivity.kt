f02d1d4c98a75784a7f819dd007a8d7235758c6c
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2024-10-21 13:51:45 -0500

    modificación del icono

--- app/src/main/java/com/example/lab12_gm/MainActivity.kt
+++ app/src/main/java/com/example/lab12_gm/MainActivity.kt
@@ -13,35 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab12_gm.ui.theme.Lab12_gmTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Lab12_gmTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+    class MainActivity : ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContent {
+                MapScreen()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab12_gmTheme {
-        Greeting("Android")
-    }
-}
