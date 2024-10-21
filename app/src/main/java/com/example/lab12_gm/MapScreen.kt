a2a47969ac97c8602ee0a1edf5274c825dea656d
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2024-10-21 14:42:54 -0500

    implementación de polígonos

--- app/src/main/java/com/example/lab12_gm/MapScreen.kt
+++ app/src/main/java/com/example/lab12_gm/MapScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -17,6 +18,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polygon
 
 
 @Composable
@@ -25,6 +27,27 @@ fun MapScreen() {
         LatLng(-16.433415,-71.5442652), // JLByR
         LatLng(-16.4205151,-71.4945209), // Paucarpata
         LatLng(-16.3524187,-71.5675994) // Zamacola
+    )
+    // Lista de coordenadas para los polígonos
+    val mallAventuraPolygon = listOf(
+        LatLng(-16.432292, -71.509145),
+        LatLng(-16.432757, -71.509626),
+        LatLng(-16.433013, -71.509310),
+        LatLng(-16.432566, -71.508853)
+    )
+
+    val parqueLambramaniPolygon = listOf(
+        LatLng(-16.422704, -71.530830),
+        LatLng(-16.422920, -71.531340),
+        LatLng(-16.423264, -71.531110),
+        LatLng(-16.423050, -71.530600)
+    )
+
+    val plazaDeArmasPolygon = listOf(
+        LatLng(-16.398866, -71.536961),
+        LatLng(-16.398744, -71.536529),
+        LatLng(-16.399178, -71.536289),
+        LatLng(-16.399299, -71.536721)
     )
 
     val cameraPositionState = rememberCameraPositionState {
@@ -42,22 +65,30 @@ fun MapScreen() {
             val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.monicon)
             val smallMarkerIcon = Bitmap.createScaledBitmap(bitmap, 84, 84, false) // Redimensiona el bitmap
             val markerDescriptor = BitmapDescriptorFactory.fromBitmap(smallMarkerIcon)
-            // Añadir marcador en Arequipa Perú
-            locations.forEach { location ->
-                Marker(
-                    state = rememberMarkerState(position = location),
-                    icon = markerDescriptor, // Icono personalizado
-                    title = "Arequipa, Perú"
-                )
-            }
+
+
+            // Dibujar polígonos
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue, // Relleno con transparencia
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
 
         }
     }
-    LaunchedEffect(Unit) {
-        cameraPositionState.animate(
-            update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984,-71.6836503), 12f), // Mover a Yura
-            durationMs = 3000
-        )
-    }
+
 
 }
